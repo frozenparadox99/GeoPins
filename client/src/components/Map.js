0ad956ef52a4c3bc62be89bda59aaa9a415f5034
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-12 22:30:14 +0530

    added delete functionality for each pin created by a user

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -9,6 +9,7 @@ import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
 import { useClient } from "../client";
 import { GET_PINS_QUERY } from "../graphql/queries";
+import { DELETE_PIN_MUTATION } from "../graphql/mutations";
 
 import Context from "../context";
 
@@ -85,6 +86,13 @@ const Map = ({ classes }) => {
 
   const isAuthUser = () => state.currentUser._id === popup.author._id;
 
+  const handleDeletePin = async (pin) => {
+    const variables = { pinId: pin._id };
+    const { deletePin } = await client.request(DELETE_PIN_MUTATION, variables);
+    dispatch({ type: "DELETE_PIN", payload: deletePin });
+    setPopup(null);
+  };
+
   return (
     <div className={classes.root}>
       <ReactMapGL
@@ -158,7 +166,7 @@ const Map = ({ classes }) => {
                 {popup.latitude.toFixed(6)},{popup.longitude.toFixed(6)}
               </Typography>
               {isAuthUser() && (
-                <Button>
+                <Button onClick={() => handleDeletePin(popup)}>
                   <DeleteIcon className={classes.deleteIcon} />
                 </Button>
               )}
