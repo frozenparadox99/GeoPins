a51d6ea2e02b8e98348fdec55f506e88c0ce2e68
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-13 13:26:35 +0530

    added CraeteComment component

--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -6,6 +6,9 @@ import FaceIcon from "@material-ui/icons/Face";
 
 import format from "date-fns/format";
 
+import CreateComment from "../Comment/CreateComment";
+import Comments from "../Comment/Comments";
+
 import Context from "../../context";
 
 const PinContent = ({ classes }) => {
@@ -39,6 +42,9 @@ const PinContent = ({ classes }) => {
       <Typography variant="subtitle1" gutterBottom>
         {content}
       </Typography>
+
+      <CreateComment />
+      <Comments comments={comments} />
     </div>
   );
 };
